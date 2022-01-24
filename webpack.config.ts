0cd7e96570400f9d218240115fde3e8a9c6f0d26
Tyler Long <tyler4long@gmail.com>
2022-01-24 14:12:11 -0800

    Initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,40 @@
+/* eslint-disable node/no-unpublished-import */
+import {Configuration} from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+
+const config: Configuration = {
+  devtool: 'source-map',
+  mode: 'development',
+  entry: {
+    index: './src/index.tsx',
+  },
+  output: {
+    path: path.join(__dirname, 'docs'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'ts-loader',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Virtual Office',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+};
+
+export default config;
