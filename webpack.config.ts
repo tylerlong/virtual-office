aa2d1a224c871d0243123a0711410bc65c70fbab
Tyler Long <tyler4long@gmail.com>
2022-01-25 18:17:46 -0800

    Meeting is working

--- webpack.config.ts
+++ webpack.config.ts
@@ -4,45 +4,57 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import dotenv from 'dotenv-override-true';
 
-const config: Configuration = {
-  devtool: 'source-map',
-  mode: 'development',
-  entry: {
-    index: './src/index.tsx',
-  },
-  output: {
-    path: path.join(__dirname, 'docs'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
+const f = (env: {}, argv: Configuration): Configuration => {
+  const config: Configuration = {
+    devtool: 'source-map',
+    mode: 'development',
+    entry: {
+      index: './src/index.tsx',
+    },
+    output: {
+      path: path.join(__dirname, 'docs'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'ts-loader',
+          },
         },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(jpeg|webp)$/,
-        type: 'asset/resource',
-      },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(jpeg|webp)$/,
+          type: 'asset/resource',
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: 'Virtual Office',
+        meta: {
+          'origin-trial': {
+            'http-equiv': 'origin-trial',
+            content:
+              argv.mode === 'production'
+                ? 'AlzgT2e7AUfad0+/eTgcFIJ9Qr1j6eAnHZtqvGGRNsRSDiDGwFdF9ABhott7a/03p++Dcy48oJx+VvUPkw+/PwcAAABoeyJvcmlnaW4iOiJodHRwczovL2NodW50YW9saXUuY29tOjQ0MyIsImZlYXR1cmUiOiJSVENFeHRlbmREZWFkbGluZUZvclBsYW5CUmVtb3ZhbCIsImV4cGlyeSI6MTY1MzQzNjc5OX0='
+                : 'AiobAcmMQ6Z01QsBkPln9CP5oSPo9EJsBHB0ef+XUoZh/wT719hTGgWf/g4n3s0OS5FiPkCbN1fk482lKjTJaQwAAABjeyJvcmlnaW4iOiJodHRwOi8vbG9jYWxob3N0OjgwODAiLCJmZWF0dXJlIjoiUlRDRXh0ZW5kRGVhZGxpbmVGb3JQbGFuQlJlbW92YWwiLCJleHBpcnkiOjE2NTM0MzY3OTl9',
+          },
+        },
+      }),
+      new DefinePlugin({
+        'process.env': JSON.stringify(dotenv.config().parsed),
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Virtual Office',
-    }),
-    new DefinePlugin({
-      'process.env': JSON.stringify(dotenv.config().parsed),
-    }),
-  ],
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-  },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+  };
+  return config;
 };
 
-export default config;
+export default f;
